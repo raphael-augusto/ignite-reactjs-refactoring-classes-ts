c4ff453ef70725c5d005ef8702dfd4f66d17ed5d
Raphael Augusto <raphaelaugusto@tutanota.com>
2021-03-27 02:38:04 -0300

    feat: refactoring to typescript

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import { Routes } from './routes';
+
+import GlobalStyle from './styles/global';
+
+export const App = () => (
+  <>
+    <GlobalStyle />
+    <Router>
+      <Routes />
+    </Router>
+  </>
+);
+
+
